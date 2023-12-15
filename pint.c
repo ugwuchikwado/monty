97554df05a6367bc25c786b38e8c3a5f73333864
Chikwado <ugwuchikwadoaugustine@gmail.com>
2023-12-15 14:08:07 +0100

    Update pint.c

--- pint.c
+++ pint.c
@@ -14,7 +14,7 @@ void pint(stack_t **stack, unsigned int line_cnt)
 {
 	if (!stack || !(*stack))
 	{
-		fprintf(stderr, "L%d: operation impossible, stack empty\n", line_cnt);
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_cnt);
 		exit(EXIT_FAILURE);
 
 	}
